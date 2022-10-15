ac29560da60fd12f0826772af6595ae9f5377e2b
Yaroslav <yarikmeisan@yandex.com>
2022-10-15 18:50:51 +0300

    Paste firebaseConfig{} to login.js

---
+++ js/login.js
@@ -0,0 +1,12 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyDYo2MHCzQ7e5VWjVa4sSRJDWAFZHVQkpw",
+    authDomain: "letsmakeagame-13447.firebaseapp.com",
+    projectId: "letsmakeagame-13447",
+    storageBucket: "letsmakeagame-13447.appspot.com",
+    messagingSenderId: "492057155348",
+    appId: "1:492057155348:web:121c7cb8658f31fe369401",
+    measurementId: "G-M10XX1QCG0"
+};
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
