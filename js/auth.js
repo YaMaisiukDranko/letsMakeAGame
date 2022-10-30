43af43e182cdda729abfa3ce5ac45af5e592df9a
Yaroslav <yarikmeisan@yandex.com>
2022-10-30 15:16:32 +0300

    Setting up saving user

--- js/auth.js
+++ js/auth.js
@@ -1,9 +1,12 @@
 //sign up
-const signupForm = document.querySelector('#signup-form')
+const signupForm = document.querySelector('#signup')
 
 signupForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     //Get user info
     const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+
+    console.log(email, password);
 })
