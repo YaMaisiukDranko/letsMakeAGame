ac3c6d331e893fb8e609a913fff146c4817fc1c0
Yaroslav <yarikmeisan@yandex.com>
2022-10-30 14:15:30 +0300

    Setting up getting user email

--- js/auth.js
+++ js/auth.js
@@ -1,2 +1,9 @@
 //sign up
 const signupForm = document.querySelector('#signup-form')
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    //Get user info
+    const email = signupForm['signup-email'].value;
+})
