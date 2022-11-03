53eff5c747ed59ac575317e7bf5bf90ef8fcb047
Yaroslav <yarikmeisan@yandex.com>
2022-11-03 19:00:07 +0300

    Add onAuthStateChanged firebase code

--- js/auth.js
+++ js/auth.js
@@ -1,6 +1,21 @@
-function login(){
-    window.alert("login button")
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        // ...
+    } else {
+        // User is signed out
+        // ...
+    }
+});
+
+function login(){
     var userEmail = document.getElementById("email-field").value;
     var userPassword = document.getElementById("password-field").value;
+
+    window.alert("login button" + userEmail + userPassword);
 }
