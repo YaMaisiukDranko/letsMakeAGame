149b15098c9d5310fd47864f877d63866ce46815
Yaroslav <yarikmeisan@yandex.com>
2022-11-03 18:55:21 +0300

    Add userEmail and userPassword variables

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,6 @@
 function login(){
     window.alert("login button")
+
+    var userEmail = document.getElementById("email-field").value;
+    var userPassword = document.getElementById("password-field").value;
 }
